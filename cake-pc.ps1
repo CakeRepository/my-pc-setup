7a4f5b89bbf2d8338bb83951b3daece96d274f20
Justin Trantham <takescake@outlook.com>
2025-03-16 19:26:06 -0500

    Update cake-pc.ps1

--- cake-pc.ps1
+++ cake-pc.ps1
@@ -1,1 +1,3 @@
-winget install DesiredStateConfiguration 
+mkdir c:\pc-setup\
+
+winget install --id Microsoft.DesiredStateConfiguration --accept-source-agreements --accept-package-agreements --silent > C:\pc-setup\DSCInstall.log 2>&1
